11797cfb13fc53430f9d091cb92d4ebd9805e529
Paulos Tesfazghi <71388869+pth2020@users.noreply.github.com>
2024-02-26 23:47:03 +0000

    Update details.py

--- variables/details.py
+++ variables/details.py
@@ -1,1 +1,23 @@
+"""
+Psedo code starts here:
 
+request input (name) from user
+store input into variable called "name"
+request input (age) from user
+store input into variable called "age"
+request input (house numer) from user
+store input into variable called "house_number"
+request input (street name) from user
+store input into variable called "street_name"
+output a single statement containing name, age, 
+house number and street name of user
+
+Psedo code ends here.
+"""
+#Collect data from user
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+house_number = input("Enter your house number: ")
+street_name = input("Enter your street name: ")
+
+print(f"This is {name}. He/she is {age} years old and lives at house number {house_number} on {street_name}.")
