bfe469da8ad756cb66ae6fb6221d1b85c2285a85
Paulos Tesfazghi <71388869+pth2020@users.noreply.github.com>
2024-02-26 23:58:59 +0000

    Update strip.py

--- Strings/strip.py
+++ Strings/strip.py
@@ -1,1 +1,8 @@
+hero = "$$$Superman$$$"
 
+#hero before being stripped off $
+print("Before stripped:",hero)
+
+#hero after being stripped off $
+hero_strip = hero.strip("$")
+print("After stripped:",hero_strip)
