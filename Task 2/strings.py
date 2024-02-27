c3f3e47bb8afe510c54fbb73b5e5ecb08a27d570
Paulos Tesfazghi <71388869+pth2020@users.noreply.github.com>
2024-02-27 09:49:55 +0000

    Delete Task 2 directory

--- Task 2/strings.py
+++
@@ -1,9 +0,0 @@
-hero = "$$$Superman$$$"
-
-#hero before being stripped off $
-print("Before stripped:",hero)
-
-#hero after being stripped off $
-hero_strip = hero.strip("$")
-print("After stripped:",hero_strip)
-
